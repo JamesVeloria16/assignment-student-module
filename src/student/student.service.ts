c89de0fa3614a8e845ade71c646b00b9ca0555c2
JamesVeloria16 <jamesmatthew.veloria@cvsu.edu.ph>
2025-01-11 10:18:47 +0800

    U

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -27,4 +27,10 @@ export class StudentsService {
         return student;
       }
       
+      async updateStudent(id: number, data: Partial<Student>): Promise<Student> {
+        const student = await this.getStudentById(id);
+        Object.assign(student, data);
+        return this.studentRepository.save(student);
+      }
+      
 }
